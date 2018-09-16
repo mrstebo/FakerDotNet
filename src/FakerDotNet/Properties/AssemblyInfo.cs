de90e7bafac0c483f104d87cbc2f6cc314923cc4
Steven Atkinson <steven.atkinson@gmx.com>
2018-09-17 00:00:34 +0100

    Added the GenerateAssemblyInfo option to the csproj file.

--- src/FakerDotNet/Properties/AssemblyInfo.cs
+++ src/FakerDotNet/Properties/AssemblyInfo.cs
@@ -1,8 +1,17 @@
 using System.Reflection;
 using System.Runtime.CompilerServices;
 
-// [assembly: AssemblyVersion("1.0.0.0")]
-// [assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyTitle("FakerDotNet")]
+[assembly: AssemblyDescription("A .NET port of the Ruby faker gem.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("mrstebo")]
+[assembly: AssemblyProduct("FakerDotNet")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: InternalsVisibleTo("FakerDotNet.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]

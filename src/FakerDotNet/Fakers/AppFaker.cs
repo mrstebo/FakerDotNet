7ea5ee6f3b143455f11200aefd8e7fb28f8d79f2
Steven Atkinson <steven.atkinson@gmx.com>
2018-09-20 21:04:44 +0100

    Implemented the App faker with tests.

---
+++ src/FakerDotNet/Fakers/AppFaker.cs
@@ -0,0 +1,43 @@
+using System.Linq;
+using System.Text.RegularExpressions;
+using FakerDotNet.Data;
+
+namespace FakerDotNet.Fakers
+{
+    public interface IAppFaker
+    {
+        string Name();
+        string Version();
+        string Author();
+    }
+    
+    internal class AppFaker : IAppFaker
+    {
+        private static readonly AppData Data = new AppData();
+        
+        private readonly IFakerContainer _fakerContainer;
+
+        public AppFaker(IFakerContainer fakerContainer)
+        {
+            _fakerContainer = fakerContainer;
+        }
+
+        public string Name()
+        {
+            return _fakerContainer.Random.Element(Data.Names);
+        }
+
+        public string Version()
+        {
+            var numbers = Enumerable.Range(0, 9);
+            var format = _fakerContainer.Random.Element(Data.Versions);
+
+            return Regex.Replace(format, "#", m => _fakerContainer.Random.Element(numbers).ToString());
+        }
+
+        public string Author()
+        {
+            return $"{_fakerContainer.Name.FirstName()} {_fakerContainer.Name.LastName()}";
+        }
+    }
+}

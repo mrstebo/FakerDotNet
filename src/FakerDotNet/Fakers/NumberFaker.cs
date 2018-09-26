1aefbee1cd06800f01ef395b8b5741e62337e638
Steve Atkinson <steve.atkinson@dootrix.com>
2018-09-26 08:38:42 +0100

    Added additional methods and started deferring some of the IRandomWrapper calls to the base implementation.

--- src/FakerDotNet/Fakers/NumberFaker.cs
+++ src/FakerDotNet/Fakers/NumberFaker.cs
@@ -10,6 +10,12 @@ namespace FakerDotNet.Fakers
         long Number(int digits = 10);
         decimal Decimal(int leftDigits = 5, int rightDigits = 2);
         double Normal(int mean = 1, int standardDeviation = 1);
+        int Digit();
+        string Hexadecimal(int digits = 6);
+        double Between(double min, double max);
+        double Within(Range<double> range);
+        double Positive(double from = 1.00D, double to = 5000.00D);
+        double Negative(double from = -5000.00D, double to = -1.00D);
     }
 
     internal class NumberFaker : INumberFaker
@@ -47,6 +53,39 @@ namespace FakerDotNet.Fakers
             var rho = Math.Sqrt(-2 * Math.Log(1 - +_randomWrapper.NextDouble()));
             var scale = standardDeviation * rho;
             return mean + scale * Math.Cos(theta);
+        }
+
+        public int Digit()
+        {
+            return _randomWrapper.Next(0, 9);
+        }
+
+        public string Hexadecimal(int digits = 6)
+        {
+            return string.Join("", Enumerable.Range(0, digits).Select(_ => Digit().ToString("X")));
+        }
+
+        public double Between(double min, double max)
+        {
+            var realMin = Math.Min(min, max);
+            var realMax = Math.Max(min, max);
+
+            return _randomWrapper.NextDouble() * (realMax - realMin) + realMin;
+        }
+
+        public double Within(Range<double> range)
+        {
+            return Between(range.Minimum, range.Maximum);
+        }
+
+        public double Positive(double from = 1.00D, double to = 5000.00D)
+        {
+            return Between(from, to);
+        }
+
+        public double Negative(double from = -5000.00D, double to = -1.00D)
+        {
+            return Between(from, to);
         }
     }
 }
--- tests/FakerDotNet.Tests/Fakers/NumberFakerTests.cs
+++ tests/FakerDotNet.Tests/Fakers/NumberFakerTests.cs
@@ -1,5 +1,7 @@
+using System.Text.RegularExpressions;
 using FakeItEasy;
 using FakerDotNet.Fakers;
+using FakerDotNet.Tests.Helpers;
 using FakerDotNet.Wrappers;
 using NUnit.Framework;
 
@@ -12,12 +14,24 @@ namespace FakerDotNet.Tests.Fakers
         [SetUp]
         public void SetUp()
         {
-            _randomWrapper = A.Fake<IRandomWrapper>();
+            _randomWrapper = A.Fake<RandomWrapper>(options => options.Implements<IRandomWrapper>().CallsBaseMethods());
             _numberFaker = new NumberFaker(_randomWrapper);
         }
 
         private IRandomWrapper _randomWrapper;
         private INumberFaker _numberFaker;
+
+        [Test]
+        public void Between_returns_number_between_min_and_max()
+        {
+            100.Times(() =>
+            {
+                var result = _numberFaker.Between(0, 100);
+
+                Assert.GreaterOrEqual(result, 0);
+                Assert.LessOrEqual(result, 100);
+            });
+        }
 
         [Test]
         public void Decimal_returns_number_with_five_left_digits_and_two_right_digits_by_default()
@@ -44,6 +58,44 @@ namespace FakerDotNet.Tests.Fakers
             A.CallTo(() => _randomWrapper.Next(0, 9)).Returns(9);
 
             Assert.AreEqual(19999.199999, _numberFaker.Decimal(5, 6));
+        }
+
+        [Test]
+        public void Digit_returns_a_single_digit()
+        {
+            100.Times(() =>
+            {
+                var result = _numberFaker.Digit();
+
+                Assert.GreaterOrEqual(result, 0);
+                Assert.LessOrEqual(result, 9);
+            });
+        }
+
+        [Test]
+        public void Hexadecimal_returns_a_hex_string_with_six_digits_by_default()
+        {
+            100.Times(() =>
+            {
+                var result = _numberFaker.Hexadecimal();
+
+                Assert.That(Regex.IsMatch(result, @"^[a-f0-9]{6}$"));
+            });
+        }
+
+        [Test]
+        [TestCase(3)]
+        [TestCase(5)]
+        [TestCase(10)]
+        [TestCase(18)]
+        public void Hexadecimal_with_digits_returns_hex_string_with_number_of_digits(int digits)
+        {
+            100.Times(() =>
+            {
+                var result = _numberFaker.Hexadecimal(digits);
+
+                Assert.That(Regex.IsMatch(result, $@"^[a-f0-9]{{{digits}}}$"));
+            });
         }
 
         [Test]
@@ -77,6 +129,30 @@ namespace FakerDotNet.Tests.Fakers
             A.CallTo(() => _randomWrapper.Next(0, 9)).Returns(0);
 
             Assert.AreEqual(expected, _numberFaker.Number(digits));
+        }
+
+        [Test]
+        public void Within_returns_number_within_range()
+        {
+            100.Times(() =>
+            {
+                var result = _numberFaker.Within(new Range<double>(-100, 100));
+
+                Assert.GreaterOrEqual(result, -100);
+                Assert.LessOrEqual(result, 100);
+            });
+        }
+
+        [Test]
+        public void Positive_returns_positive_number()
+        {
+            100.Times(() =>
+            {
+                var result = _numberFaker.Positive();
+
+                Assert.GreaterOrEqual(result, 1D);
+                Assert.LessOrEqual(result, 5000D);
+            });
         }
     }
 }

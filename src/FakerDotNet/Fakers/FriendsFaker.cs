0e5427c35f760bc52c46f84b81b4505d2380f2b1
Jamie Introcaso <jlintrocaso@gmail.com>
2018-10-12 23:17:37 -0400

    added friends faker and tests

---
+++ src/FakerDotNet/Fakers/FriendsFaker.cs
@@ -0,0 +1,38 @@
+﻿using FakerDotNet.Data;
+
+namespace FakerDotNet.Fakers
+{
+    public interface IFriendsFaker
+    {
+        string Character();
+        string Location();
+        string Quote();
+    }
+
+    internal class FriendsFaker : IFriendsFaker
+    {
+        private static readonly FriendsData Data = new FriendsData();
+
+        private readonly IFakerContainer _fakerContainer;
+
+        public FriendsFaker(IFakerContainer fakerContainer)
+        {
+            _fakerContainer = fakerContainer;
+        }
+
+        public string Character()
+        {
+            return _fakerContainer.Random.Element(Data.Characters);
+        }
+
+        public string Location()
+        {
+            return _fakerContainer.Random.Element(Data.Locations);
+        }
+
+        public string Quote()
+        {
+            return _fakerContainer.Random.Element(Data.Quotes);
+        }
+    }
+}
---
+++ tests/FakerDotNet.Tests/Fakers/FriendsFakerTests.cs
@@ -0,0 +1,59 @@
+﻿using FakeItEasy;
+using FakerDotNet.Data;
+using FakerDotNet.Fakers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FakerDotNet.Tests.Fakers
+{
+    [TestFixture]
+    [Parallelizable]
+    public class FriendsFakerTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _fakerContainer = A.Fake<IFakerContainer>();
+            _friendsFaker = new FriendsFaker(_fakerContainer);
+        }
+
+        private static readonly FriendsData Data = new FriendsData();
+
+        private IFakerContainer _fakerContainer;
+        private IFriendsFaker _friendsFaker;
+
+        [Test]
+        public void Character_returns_a_character()
+        {
+            A.CallTo(() => _fakerContainer.Random.Element(
+                    A<IEnumerable<string>>.That.IsSameSequenceAs(Data.Characters)))
+                .Returns("Rachel Green");
+
+            Assert.AreEqual("Rachel Green", _friendsFaker.Character());
+        }
+
+        [Test]
+        public void Location_returns_a_location()
+        {
+            A.CallTo(() => _fakerContainer.Random.Element(
+                    A<IEnumerable<string>>.That.IsSameSequenceAs(Data.Locations)))
+                .Returns("Ross' Second Apartment");
+
+            Assert.AreEqual("Ross' Second Apartment", _friendsFaker.Location());
+        }
+
+        [Test]
+        public void Quote_returns_a_quote()
+        {
+            A.CallTo(() => _fakerContainer.Random.Element(
+                    A<IEnumerable<string>>.That.IsSameSequenceAs(Data.Quotes)))
+                .Returns("SEVEN!");
+
+            Assert.AreEqual("SEVEN!", _friendsFaker.Quote());
+        }
+    }
+}

2a68a1f79552eb7f310f934671f13efbe517a970
snooze <snoozins@gmail.com>
2018-10-13 11:54:37 +1100

    Started Vehicle Faker: includes Make/Model/Vin

---
+++ src/FakerDotNet/Fakers/VehicleFaker.cs
@@ -0,0 +1,88 @@
+﻿using FakerDotNet.Data;
+using System.Collections.Generic;
+
+namespace FakerDotNet.Fakers
+{
+    public interface IVehicleFaker
+    {
+        string Vin();
+        string Manufacture();
+        string Make();
+    }
+
+    internal class VehicleFaker : IVehicleFaker
+    {
+        private static readonly VehicleData Data = new VehicleData();
+
+        private readonly IFakerContainer _fakerContainer;
+
+        public VehicleFaker(IFakerContainer fakerContainer)
+        {
+            this._fakerContainer = fakerContainer;
+        }
+
+        public string Make()
+        {
+            return _fakerContainer.Random.Element(Data.Makes);
+        }
+
+        public string Manufacture()
+        {
+            return _fakerContainer.Random.Element(Data.Manufactures);
+        }
+
+        public string Vin()
+        {
+            string vin = string.Empty;
+            for (var i = 0; i < 8; i ++)
+            {
+                var e = _fakerContainer.Random.Element(vinDigitValues);
+                vin += e.Key;
+            }
+            vin += '0';
+            for (var i = 0; i < 8; i++)
+            {
+                vin += _fakerContainer.Random.Element(vinDigitValues).Key;
+            }
+            var checksum = GetChecksum(vin);
+
+
+            return vin.Substring(0, 8) + checksum + vin.Substring(9, 8);
+        }
+
+        public string Model()
+        {
+            return Model(_fakerContainer.Random.Element(Data.Makes));
+        }
+        public string Model(string make)
+        {
+            return _fakerContainer.Random.Element(Data.Make_Models[make]);
+        }
+
+        public string MakeAndModel()
+        {
+            var make = Make();
+            var model = Model(make);
+            return $"{make} {model}";
+        }
+
+        int[] vinDigitPositionMultiplier = new[] { 8, 7, 6, 5, 4, 3, 2, 10, 0, 9, 8, 7, 6, 5, 4, 3, 2 };
+        Dictionary<char, int> vinDigitValues = new Dictionary<char, int>() { { 'A', 1 }, { 'B', 2 }, { 'C', 3 }, { 'D', 4 }, { 'E', 5 }, { 'F', 6 }, { 'G', 7 }, { 'H', 8 }, { 'J', 1 }, { 'K', 2 }, { 'L', 3 }, { 'M', 4 }, { 'N', 5 }, { 'P', 7 }, { 'R', 9 }, { 'S', 2 }, { 'T', 3 }, { 'U', 4 }, { 'V', 5 }, {'W', 6 }, { 'X', 7 }, { 'Y', 8 }, { 'Z', 9 }, { '1', 1 }, { '2', 2 }, { '3', 3 }, { '4', 4 }, { '5', 5 }, { '6', 6 }, { '7', 7 }, { '8', 8 }, { '9', 9 }, { '0', 0 } };
+
+        public string GetChecksum(string vin)
+        {
+            int checkSumTotal = 0;
+            var vinArray = vin.ToCharArray();
+            for (var i = 0; i < vinArray.Length; i++)
+            {
+                checkSumTotal += vinDigitValues[vinArray[i]] * vinDigitPositionMultiplier[i];
+            }
+            var remainder = checkSumTotal % 11;
+            if (remainder == 10)
+            {
+                return "X";
+            }
+            return remainder.ToString();
+        }
+    }
+}
---
+++ tests/FakerDotNet.Tests/Fakers/VehicleFakerTests.cs
@@ -0,0 +1,60 @@
+﻿using FakeItEasy;
+using FakerDotNet.Data;
+using FakerDotNet.Fakers;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace FakerDotNet.Tests.Fakers
+{
+    [TestFixture]
+    [Parallelizable]
+    public class VehicleFakerTests
+    {
+        [SetUp]
+        public void SetUp()
+        {
+            _fakerContainer = A.Fake<IFakerContainer>();
+            _vehicleFaker = new VehicleFaker(_fakerContainer);
+        }
+
+        private static readonly VehicleData Data = new VehicleData();
+
+        private IFakerContainer _fakerContainer;
+        private IVehicleFaker _vehicleFaker;
+
+        [Test]
+        public void Make_returns_a_make()
+        {
+            A.CallTo(() => _fakerContainer.Random.Element(
+                    A<IEnumerable<string>>.That.IsSameSequenceAs(Data.Makes)))
+                .Returns("Honda");
+
+            Assert.AreEqual("Honda", _vehicleFaker.Make());
+        }
+
+        [Test]
+        public void Manufacture_returns_a_manufacture()
+        {
+            A.CallTo(() => _fakerContainer.Random.Element(
+                    A<IEnumerable<string>>.That.IsSameSequenceAs(Data.Manufactures)))
+                .Returns("Lamborghini");
+
+            Assert.AreEqual("Lamborghini", _vehicleFaker.Manufacture());
+        }
+
+        [Test]
+        public void Vin_returns_a_valid_vin()
+        {
+            var fakerContainer = new FakerContainer();
+            var vin = fakerContainer.Vehicle.Vin();
+
+            Assert.AreEqual(17, vin.Length);
+            Assert.False(vin.Any(c => c == 'I' || c == 'Q' || c == 'O'));
+            Assert.IsTrue(Regex.Match(vin, @"^[A-Z0-9]{8}[X0-9][A-Z0-9]{8}", RegexOptions.IgnoreCase).Success, $"Vin was: {vin}");
+        }
+    }
+}

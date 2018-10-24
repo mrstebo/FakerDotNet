1b192dc141a1de7b74788ed3312fd768aace5465
Steven Atkinson <mrstebouk@gmail.com>
2018-10-24 14:29:35 +0100

    Implemented the ColorFaker.

---
+++ src/FakerDotNet/Data/ColorData.cs
@@ -0,0 +1,42 @@
+using System.Collections.Generic;
+
+namespace FakerDotNet.Data
+{
+    internal static class ColorData
+    {
+        public static readonly IEnumerable<string> Names = new[]
+        {
+            "red",
+            "green",
+            "blue",
+            "yellow",
+            "purple",
+            "mint green",
+            "teal",
+            "white",
+            "black",
+            "orange",
+            "pink",
+            "grey",
+            "maroon",
+            "violet",
+            "turquoise",
+            "tan",
+            "sky blue",
+            "salmon",
+            "plum",
+            "orchid",
+            "olive",
+            "magenta",
+            "lime",
+            "ivory",
+            "indigo",
+            "gold",
+            "fuchsia",
+            "cyan",
+            "azure",
+            "lavender",
+            "silver"
+        };
+    }
+}

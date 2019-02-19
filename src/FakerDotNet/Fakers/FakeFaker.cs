732e182a017d5f0df52cbb7e867dcc128169c35b
Steven Atkinson <steven.atkinson@gmx.com>
2019-02-19 18:05:55 +0000

    Removed the FormatException if using null or an empty string.

--- src/FakerDotNet/Fakers/FakeFaker.cs
+++ src/FakerDotNet/Fakers/FakeFaker.cs
@@ -21,7 +21,7 @@ namespace FakerDotNet.Fakers
 
         public string F(string format)
         {
-            if (string.IsNullOrEmpty(format)) throw new FormatException("A string must be specified");
+            if (string.IsNullOrEmpty(format)) return string.Empty;
 
             var result = format;
             Match match;
